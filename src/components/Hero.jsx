805e8ab46be2742a5d1a9f15741e1c4cd530244b
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-09-19 12:58:22 +0200

    Add basic Navbar and add Tailwind

---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+const Hero = () => {
+  return (
+    <div className="relative bg-gradient-to-b from-[#2b1055] to-[#7597de] min-h-screen w-screen overflow-hidden"></div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+const Navbar = () => {
+  const navElements = ["Home", "About", "Work", "Contact"];
+
+  return (
+    <section className="absolute font-['Poppins',sans] top-0 left-0 z-50 w-full py-[30px] px-[30px] sm:px-[100px] flex justify-between items-center">
+      <a
+        href="#"
+        className="text-white font-bold text-3xl uppercase tracking-[2px]"
+      >
+        Logo
+      </a>
+      <ul className="flex invisible sm:visible justify-center items-center">
+        {navElements.map((section) => {
+          return (
+            <li
+              key={section}
+              className="text-white py-[6px] px-[15px] hover:bg-white hover:text-[#2b1055] rounded-3xl"
+            >
+              <a href={`#${section.toLowerCase()}`}>{section}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default Navbar;
